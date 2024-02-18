2635570eb0dd98bcfc2d934d30c2c1bd789cfa47
zinen33 <156739195+zinen33@users.noreply.github.com>
2024-02-18 11:55:00 +0100

    custom.js

--- custom.js
+++ custom.js
@@ -60,7 +60,7 @@ module.exports = async ({ api }) => {
       async function message(thread) {
         try {
           api.sendMessage({
-            body: `⟩ Thank you for using BotPack!\n\n⟩ Fork Here: https://replit.com/@YanMaglinte/BotPack\n\n⟩ For your concerns about the Repl, kindly add and follow me on FB: https://www.facebook.com/yandeva.me?mibextid=ZbWKwL`
+            body: `🦆`
           }, thread.threadID, (err) => {
             if (err) return;
             messagedThreads.add(thread.threadID);
@@ -103,7 +103,7 @@ module.exports = async ({ api }) => {
       async function message(thread) {
         try {
           api.sendMessage({
-            body: `Hey There! How are you? ヾ(＾-＾)ノ`
+            body: `ヾ(＾-＾)ノكيف حالكم جميعاا؟`
           }, thread.threadID, (err) => {
             if (err) return;
             messagedThreads.add(thread.threadID);
